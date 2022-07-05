7577f348f12c84f5635e8e5f233d4c3661c69c03
n-pacis <pacisnkubito@gmail.com>
2022-07-05 18:08:25 +0200

    Update BirthdayConnectionController.java

--- src/main/java/rw/automation/birthdayWishing/v1/controllers/BirthdayConnectionController.java
+++ src/main/java/rw/automation/birthdayWishing/v1/controllers/BirthdayConnectionController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import rw.automation.birthdayWishing.v1.dtos.RequestBirthdayConnectionDTO;
@@ -19,6 +20,7 @@ import rw.automation.birthdayWishing.v1.services.IUserService;
 import rw.automation.birthdayWishing.v1.utils.Constants;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.UUID;
 
 
